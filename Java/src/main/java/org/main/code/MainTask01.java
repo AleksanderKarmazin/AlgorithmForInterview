0fd62102e5541688822a2acf9b88144a4057d37d
Aleksandr <kubartphil@gmail.com>
2022-10-23 14:35:08 +0300

    feat: Java set up

---
+++ Java/src/main/java/org/main/code/MainTask01.java
@@ -0,0 +1,7 @@
+package org.main.code;
+
+public class MainTask01 {
+    public void mainCodeTask01() {
+        System.out.println("This is main code of task N 1 ");
+    }
+}
---
+++ Java/src/test/java/org/main/code/TestTask01.java
@@ -0,0 +1,21 @@
+package org.main.code;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTask01 {
+    @Test
+    public void testN1() {
+        MainTask01 ex = new MainTask01();
+        ex.mainCodeTask01();
+    }
+    @Test
+    public void testN2() {
+        MainTask01 ex = new MainTask01();
+        ex.mainCodeTask01();
+    }
+    @Test
+    public void testN3() {
+        MainTask01 ex = new MainTask01();
+        ex.mainCodeTask01();
+    }
+}
