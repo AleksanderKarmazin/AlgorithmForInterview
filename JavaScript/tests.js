d106338d81b427cbe357ef8baa1d56b0ab919b75
Aleksandr <kubartphil@gmail.com>
2022-10-23 15:40:58 +0300

    fix: file deletion

--- JavaScript/tests.js
+++
@@ -1,7 +0,0 @@
-console.log("This project is dedicated to solving simple algorithmic tasks for programming interviews.")
-
-
-// npx mocha --reporter spec
-// "scripts": {
-//     "test": "node tests.js"
-//   },
